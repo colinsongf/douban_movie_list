0f6492238f2fd756c147e68b71b6e00dcb9bf88f
Li Ding <ldeanego@gmail.com>
2015-05-17 19:11:02 +0800

    Comments added

--- red_heart_song_list.py
+++ red_heart_song_list.py
@@ -1,5 +1,6 @@
 # -*- coding:utf-8 -*-
 #coding=utf-8 
+__author__="Li Ding"
 
 import urllib
 import urllib2
@@ -10,9 +11,10 @@ import webbrowser
 
 class RHS:
  
-    def __init__(self):
+    def __init__(self,email,password):
         self.loginUrl = "https://www.douban.com/accounts/login"
-        # self.cookies = cookielib.CookieJar()
+        
+        #Proxy URL, in case your own id is banned by douban.fm
         self.proxyURL = 'http://61.233.25.166:80'
         self.loginHeaders =  {
             'Host':'accounts.douban.com',
@@ -24,91 +26,77 @@ class RHS:
         }
         self.post = {
             'source':'None',
-            'form_email':'uare@sina.com',
-            'form_password':'8271152',
+            'form_email':email,
+            'form_password':password,
             'remember':'on',
             'login':'登陆',
             'redir':'https://www.douban.com',
             'captcha-solution':'fiction',
             'captcha-id':'Qqa1NTqCwr1xk5Vb9VZk9KpA:en'
         }
-        
 
+
+        #Construct the cookie used to crawl douban related webpage
         self.postdata = urllib.urlencode(self.post)
         self.proxy = urllib2.ProxyHandler({'http':self.proxyURL})
         self.cookie = cookielib.LWPCookieJar()
         self.cookieHandler = urllib2.HTTPCookieProcessor(self.cookie)
         self.opener = urllib2.build_opener(self.cookieHandler,self.proxy,urllib2.HTTPHandler)
 
-        self.newCookie = cookielib.CookieJar()
-        self.newOpener = urllib2.build_opener(urllib2.HTTPCookieProcessor(self.newCookie))
+        # self.newCookie = cookielib.CookieJar()
+        # self.newOpener = urllib2.build_opener(urllib2.HTTPCookieProcessor(self.newCookie))
 
 
-        
+        #Login with Image Code
         self.loginWithIdenCode()
 
     def loginWithIdenCode(self):
+
+        #Attempt to login in, for the first try
         request = urllib2.Request(self.loginUrl,self.postdata,self.loginHeaders)
         response = self.opener.open(request)
+
         content = response.read()
-        # print content
 
     
 
         with open("first_try.html",'w') as cf:
             cf.write(content)
 
-        pattern = re.compile('<img id="captcha_image" src="(.*?)"',re.S)
-
-        # print content
-        
+        #See if Image Code is needed
+        pattern = re.compile('<img id="captcha_image" src="(.*?)"',re.S)       
         matchResult = re.search(pattern,content)
 
         if matchResult:
             print "Image Code Needed."
             idenCodeSrc= matchResult.group(1)
-
-
-            # pattern2 = re.compile('id=(.*?)&amp',re.S)
-            # idenCodeId = re.search(pattern2,idenCodeSrc)
-            # idenCodeId = idenCodeId.group(1)
-                        
-
-            # webbrowser.open_new_tab(idenCodeSrc)
-            
-            # self.post['captcha-id'] = idenCodeId
-
             
             while True:
+                #Try to login with image code
                 if self.TryInputIdenCode(idenCodeSrc):
                     break
-        else:
-            print "No Image Code Needed"
+        else:            
+            print "No Image Code Needed. Login Successfully."
 
 
     def TryInputIdenCode(self,idenCodeSrc):
 
+        #Obtain the Image Code url
         pattern2 = re.compile('id=(.*?)&amp',re.S)
         idenCodeId = re.search(pattern2,idenCodeSrc)
         idenCodeId = idenCodeId.group(1)
-                        
-
-        webbrowser.open_new_tab(idenCodeSrc)
-            
         self.post['captcha-id'] = idenCodeId
-
+                        
+        #Show the image in the web-browser and type in it in the command line manually
+        webbrowser.open_new_tab(idenCodeSrc)    
         checkcode = raw_input('Input the Image Code: ')        
         self.post['captcha-solution'] = checkcode
-        print checkcode
         self.postdata = urllib.urlencode(self.post)
 
-        # self.loginUrl="http://accounts.douban.com/login?uid=51431818&alias=uare%40sina.com&redir=https%3A%2F%2Fwww.douban.com%2F&source=None&error=1011"
-
+        #Check if the Image Code typed from user is correct
         request = urllib2.Request(self.loginUrl,self.postdata,self.loginHeaders)
         response = self.opener.open(request)
-
         content = response.read()
-        # print content
         resultpattern = re.compile('验证码不正确',re.S)
         isWrongCode = re.search(resultpattern,content)
 
@@ -121,7 +109,7 @@ class RHS:
 
             matchResult = re.search(ICpattern,content)
             idenCodeSrc= matchResult.group(1)
-
+            #Retype the Image Code
             return self.TryInputIdenCode(idenCodeSrc)
             return False
         else:
@@ -163,9 +151,8 @@ class RHS:
 
             break
        
-        # print result.read()#.decode('gbk')
  
  
-sdu = RHS()
+sdu = RHS(email="uare@sina.com",password="8271152")
 sdu.getPage()
 
